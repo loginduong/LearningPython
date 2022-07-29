5b446f2cf62648cfba336ca5530d5ec6bb6ef29f
LoganDuong <logan.duong.04@gmail.com>
2022-07-28 22:00:42 -0600

    Minor changes to FirstWidget.
    After much trying I realized that my attempts to make a label that could change by a button press were probably not going to happen due to restrictions on passing data through functions. Maybe next time.

--- main.py
+++ main.py
@@ -7,31 +7,38 @@ class FirstWidget(QWidget):
 
     # This runs when a FirstWidget is created
     def __init__(self):
-        # it runs the init of the super
+        # it runs the init of the super, which I assume is QWidget? This is beyond me at the moment.
         super().__init__()
-        # we then run the setup func of QWidget (what does that even do, man)
+
+        # I need some variables! Is this proper?!
+        self.labelle_string1 = "This text will change when you press that button"
+        self.labelle_string2 = "OOGA BOOGA YOU JUST GOT SPOOKED"
+        self.btn_changed = False
+
+        # we then run the setup func of QWidget
         self.setup()
 
     # because Python runs on black magic we can define a class after needing to use it, so here is setup()
     def setup(self):
+        """Sets up the GUI window and all the things in it"""
         # Quit button **********************************************************
         # btn_quit is a new object, we create it as a QPushButton with a label and self
-        btn_quit = QPushButton('BUTT', self)
-        # btn_quit is set so that when you click it, the instance of QApplication closes
+        btn_quit = QPushButton('Quit!', self)
 
         # btn_quit.clicked.connect(QApplication.instance().quit()) -- This runs but doesn't close when you click it
         # btn_quit.clicked().connect(QApplication.instance().quit()) -- This does not work
+
         btn_quit.clicked.connect(QApplication.instance().quit)  # okay this one works, quit doesn't need parenthesis
+        # Here (I assume) we "connect" the clicking action of the button to closing the instance of this QApp
 
         # I have no idea what this does yet
         btn_quit.resize(btn_quit.sizeHint())
-
         btn_quit.move(10, 10)
 
-        # Let's make new buttons?? **********************************************
+        # Let's make new buttons **********************************************
         btn_signal = QPushButton('Send Signal', self)
         btn_signal.setCheckable(True)
-        btn_signal.clicked.connect(self.sendSignal)
+        btn_signal.clicked.connect(self.send_signal)
         btn_signal.move(100, 30)
 
         # Labels *********************************************************************
@@ -39,14 +46,15 @@ class FirstWidget(QWidget):
         labelle1.move(100, 10)
 
         # this sets the size of the window when it appears. X pos, Y pos, width, height
-        self.setGeometry(200, 100, 300, 300)
+        self.setGeometry(200, 100, 300, 150)
         # this I assume is setting the window's title
-        self.setWindowTitle('WINDOW TITLE')
+        self.setWindowTitle('Window Title')
 
         # shows the window
         self.show()
 
     def closeEvent(self, event: QCloseEvent):
+        """Prompts the user with a message to ask if they want to close the GUI"""
         # second parameter is the question box's title, third parameter is the contents
         reply = QMessageBox.question(self, 'Message', 'Are you sure about that?',
                                      QMessageBox.Yes | QMessageBox.No, QMessageBox.No)
@@ -55,10 +63,14 @@ class FirstWidget(QWidget):
         else:
             event.ignore()
 
-    def sendSignal(self):
+    def send_signal(self):
+        """Produces an 'about' message box"""
         QMessageBox.about(self, 'Title', 'Text')
 
 
+# To be perfectly honest, the guide I was following proceeded to write this and not really elaborate what this did.
+# The program itself will show the window as a part of the setup function so what this does is currently a mystery.
+# Instantiating an instance of FirstWidget creates the GUI, but what app does is a mystery
 def run():
     app = QApplication([])
     fw = FirstWidget()
