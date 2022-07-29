9ecdc46ffa80cea1064fc62cfdbf0850f039412d
LoganDuong <logan.duong.04@gmail.com>
2022-07-28 23:35:24 -0600

    Edits to docstrings in main

--- main.py
+++ main.py
@@ -10,18 +10,19 @@ class FirstWidget(QWidget):
         # it runs the init of the super, which I assume is QWidget? This is beyond me at the moment.
         super().__init__()
 
-        # I need some variables! Is this proper?!
-        self.labelle_string1 = "This text will change when you press that button"
-        self.labelle_string2 = "OOGA BOOGA YOU JUST GOT SPOOKED"
-        self.btn_changed = False
-
         # we then run the setup func of QWidget
         self.setup()
 
     # because Python runs on black magic we can define a class after needing to use it, so here is setup()
     def setup(self):
-        """Sets up the GUI window and all the things in it"""
-        # Quit button **********************************************************
+        """
+        Sets up the GUI window and all the things in it
+
+        The unique components are:
+        btn_quit prompts the QApplication to close as if the user closed it normally
+        btn_signal is an example of connecting a button's click to a function
+        labelle1 is just a text label
+        """
         # btn_quit is a new object, we create it as a QPushButton with a label and self
         btn_quit = QPushButton('Quit!', self)
 
@@ -35,13 +36,13 @@ class FirstWidget(QWidget):
         btn_quit.resize(btn_quit.sizeHint())
         btn_quit.move(10, 10)
 
-        # Let's make new buttons **********************************************
+        # Let's make new buttons
         btn_signal = QPushButton('Send Signal', self)
         btn_signal.setCheckable(True)
         btn_signal.clicked.connect(self.send_signal)
         btn_signal.move(100, 30)
 
-        # Labels *********************************************************************
+        # Trying Labels
         labelle1 = QLabel("This is a Label", self)
         labelle1.move(100, 10)
 
@@ -54,7 +55,11 @@ class FirstWidget(QWidget):
         self.show()
 
     def closeEvent(self, event: QCloseEvent):
-        """Prompts the user with a message to ask if they want to close the GUI"""
+        """
+        Prompts the user with a message to ask if they want to close the GUI
+        By declaring event: in the arguments, presumably this allows this function to modify what happens
+        during QCloseEvent, which is unsurprisingly when this QApp closes.
+        """
         # second parameter is the question box's title, third parameter is the contents
         reply = QMessageBox.question(self, 'Message', 'Are you sure about that?',
                                      QMessageBox.Yes | QMessageBox.No, QMessageBox.No)
@@ -64,7 +69,7 @@ class FirstWidget(QWidget):
             event.ignore()
 
     def send_signal(self):
-        """Produces an 'about' message box"""
+        """Produces an 'about' message box with some sample text"""
         QMessageBox.about(self, 'Title', 'Text')
 
 
